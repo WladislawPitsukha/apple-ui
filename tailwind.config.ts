b97864330c163b0fcd614c416c18475b078bab14
Vladislav Pitsukha <pitsukhavladislav@gmail.com>
2024-12-10 00:35:47 +0100

    feat: add new details of components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,13 +16,15 @@ const config: Config = {
         foreground: "var(--foreground)",
         'navbar-color': 'rgba(22, 22, 23, 0.8)',
         'custom-white': 'rgba(245, 245, 247, 1)',
-        'custom-blue': 'rgba(41, 151, 255, 1)',
+        'custom-blue': 'rgba(41, 151, 255, 1)', 
         'custom-gray': 'rgba(110, 110, 115, 1)',
         'custom-orange': 'rgba(245, 99, 0, 1)',
         'light-white': 'rgb(255 247 247 / 85%)',
         'light-blue': 'rgba(0, 102, 204, 1)',
         'custom-dark': 'rgba(29, 29, 31, 1)',
-        'custom-dark-gray': 'rgba(66, 66, 69, 1)'
+        'custom-dark-gray': 'rgba(66, 66, 69, 1)',
+        'custom-light-gray': 'rgba(245, 245, 247, 1)',
+        'custom-red': 'rgba(238, 0, 0, 1)',
       },
       letterSpacing: {
         'custom-1': '-0.28px',
