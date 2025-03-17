842444c61bd9bfef64e691d3adf5f07ae120aee2
Vladislav Pitsukha <pitsukhavladislav@gmail.com>
2025-03-18 00:29:06 +0100

    feat: add new photos, components, array

---
+++ src/app/(services)/iphone/components/constants/arrLinksPazzle.ts
@@ -0,0 +1,13 @@
+import { FotlinkProps } from "@/app/(services)/mac/types/typeFotProps";
+
+
+export const arrLinksPazzle: FotlinkProps[] = [
+    {
+        title: "Compare all iPhone models ",
+        link: ""
+    },
+    {
+        title: "Shop iPhone",
+        link: ""
+    }
+]
